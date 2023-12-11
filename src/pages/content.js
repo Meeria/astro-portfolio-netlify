985f2fbbec9b55e216b99f61e881d7d10c518b80
Meeria Dap <meriadec.denierdaprigny@caensup.fr>
2023-12-11 22:50:33 +0100

    test #dist pour deploiement

--- src/pages/content.js
+++ src/pages/content.js
@@ -1,153 +1,205 @@
 // Personal Information --> #hero section
-export const name = "Peter Griffin"
-export const designation = "Safety Inspector"
-export const location = "Quahog, Rhode Island"
-export const pronouns = "he/him"
-export const website = "https://petergriffin.com"
+export const name = "Denier d'Aprigny Mériadec"
+export const designation = "Etudiant développeur Web"
+export const location = "France, Caen"
+//export const pronouns = "il"
+//export const website = "https://petergriffin.com"
 
 // About
-export const about = "An adventurous man working as a safety inspector with a quirky sense of humor. Husband to Louis Griffin. Father of Chris, Brian, and Stewie."
+export const about = "Etudiant en 2ème année de BTS SIO (Service Informatique aux Organisations) option SLAM (Solutions Logicielles et Applications Métiers) à CaenSup Sainte-Ursule. JAVA, JavaScript, SQL, PHP, Python, Spring, HTML, CSS"
 
 // Work Experience --> #work section
 export const work = [
 	{
-		from: "2001",
-		to: "Present",
-		role: "Safety Inspector",
-		org: "Quahog Safety Department",
-		org_url: "https://quahog.gov/safety",
-		location: "Quahog, Rhode Island",
-		skills: "Safety Inspection, Risk Assessment, Emergency Response"
+		from: "Mai 2023",
+		to: "Juin 2023",
+		role: "Stagiaire Developpeur / Data Analyst",
+		org: "Carrefour France",
+		org_url: "https://www.linkedin.com/company/carrefour-administratif-france/",
+		location: "France, Caen",
+		skills: "Développement d'un logiciel web (JS, SQL, APP SCRIPT). Aide dans letraitement de Big Data (outils google)"
+	},
+	{
+		from: "Juin 2023",
+		to: "Août 2023",
+		role: "Mise en rayon",
+		org: "Leclerc",
+		org_url: "https://www.leclerc.fr/",
+		location: "France, Saint-lô",
+		skills: "Mise en rayon en supermarché dans un temps donné"
+	},
+	{
+		from: "Mars 2022",
+		to: "Mai 2022",
+		role: "Montage équipements automobile",
+		org: "Lecapitaine",
+		org_url: "https://www.lecapitaine.com/fr-fr",
+		location: "France, Saint-lô",
+		skills: "Montage d’accessoires sur des véhicules frigorifiques"
+	},
+	{
+		from: "Juillet 2018",
+		to: "Juin 2019",
+		role: "Soldat réserviste",
+		org: "Armée de Terre",
+		org_url: "https://www.defense.gouv.fr/terre",
+		location: "France, SAINT-AUBIN DU CORMIER",
+		skills: "Soldat réserviste : tir, surveillance, activités physiques, sécurisation."
 	}
 ]
 
 // Projects --> #project section
 export const projects = [
 	{
-		title: "Project Griffin",
-		description: "A project to improve safety standards in Quahog by implementing innovative safety measures.",
-		url: "https://projectgriffin.com",
-		tech: "Safety Equipment, Training Programs, Community Outreach",
+		timeframe: "Mai-Juin 2023",
+		title: "Monitoring GDA",
+		description: "Interface permettant de suivre en direct les appels du call center de Carrefour France Administratif",
+		url: "/monitoring.html",
+		tech: "JavaScript, TypeScript, CSS, HTML, SQL, GCP, Big Query, Figma",
 	},
 	{
-		title: "Family Safety Campaign",
-		description: "An initiative to promote safety awareness among families in Quahog.",
-		url: "https://safefamilyquahog.com",
-		tech: "Workshops, Educational Material, Public Events",
+		timeframe: "2022-2023",
+		title: "Album Photos",
+		description: "Album photo intéractif avec une interface admin. Architecture MVC",
+		url: "/album_photo.html",
+		tech: "PHP, Javascript, SQL, Maria DB, CSS, HTML",
 	},
 	{
-		title: "Road Safety Initiative",
-		description: "A campaign to enhance road safety and reduce accidents in Quahog.",
-		url: "https://roadsafetyquahog.com",
-		tech: "Traffic Analysis, Signage Improvement, Public Awareness",
+		timeframe: "2022",
+		title: "Portfolio v1",
+		description: "Réalisation d'un portfolio pour prendre en main les premiers langages web",
+		url: "/portfolio.html",
+		tech: "CSS, JavaScript, HTML, SQL, Maria DB",
 	}
 ]
 
 // Education --> #education section
 export const education = [
 	{
-		from: "1995",
-		to: "1999",
-		institute: "Quahog High School",
-		course: "High School Diploma",
-		location: "Quahog, Rhode Island"
+		from: "2022",
+		to: "maintenant",
+		institute: "CaenSup Saint-Ursule",
+		course: "BTS SIO Option SLAM",
+		location: "FRANCE, Caen"
 	},
 	{
-		from: "1999",
-		to: "2003",
-		institute: "Quahog Community College",
-		course: "Bachelor of Science in Safety Management",
-		location: "Quahog, Rhode Island"
+		from: "2020",
+		to: "2021",
+		institute: "Université de CAEN",
+		course: "1er année LICENCE STAPS",
+		location: "FRANCE, Caen"
 	},
 	{
-		from: "2003",
-		to: "2005",
-		institute: "Safety University",
-		course: "Master of Science in Safety Engineering",
-		location: "Boston, Massachusetts"
+		from: "2017",
+		to: "2019",
+		institute: "Lycée Curie-Corot, SAINT-LÔ",
+		course: "BAC STI2D Option EE",
+		location: "FRANCE, Saint-lô"
 	}
 ]
 
 // Contact --> #contact section
 export const contact = [
 	{
-		source_name: "Email",
-		source: "peter.griffin@example.com",
+		source_name: "meria.dap@hotmail.fr",
+		source: "meria.dap@hotmail.fr",
 	},
 	{
-		source_name: "Twitter",
-		source: "https://twitter.com/PeterGriffin",
+		source_name: "Github",
+		source: "https://github.com/Meeria",
+	},
+	{
+		source_name: "Gitlab",
+		source: "https://gitlab.com/Meeria",
 	},
 	{
 		source_name: "LinkedIn",
-		source: "https://www.linkedin.com/in/peter-griffin",
-	},
+		source: "www.linkedin.com/in/mériadec-d-9371951ab",
+	}
 ]
 
 // Certifications --> #certificate section
 
 export const certificates = [
 	{
-		issued: "2005",
-		title: "Certified Safety Inspector",
-		org: "National Safety Association",
-		url: "https://certi.com/url",
-		description: "Certification in safety inspection and risk assessment.",
+		issued: "2023",
+		title: "Initiez-vous à Linux",
+		org: "OpenClassRooms",
+		url: "\certifOpen.PNG",
+		description: "Utiliser les bons outils pour exploiter Linux dans la vie professionnelle.",
 	},
 	{
-		issued: "2006",
-		title: "Emergency Response Training",
-		org: "Quahog Safety Department",
-		url: "https://certi.com/url",
-		description: "Training in handling emergency situations and crisis management.",
+		issued: "2023",
+		title: "Apprenez les bases du langage Python",
+		org: "OpenClassRooms",
+		url: "\certifOpen.PNG",
+		description: "",
 	},
 	{
-		issued: "2007",
-		title: "Safety Leadership Workshop",
-		org: "Safety Leaders International",
-		url: "https://certi.com/url",
-		description: "Leadership training in safety management and communication.",
+		issued: "2022",
+		title: "Apprenez à programmer avec JavaScript",
+		org: "OpenClassRooms",
+		url: "\certifOpen.PNG",
+		description: "",
+	},
+	{
+		issued: "2022",
+		title: "Concevez votre site web avec PHP et MySQL",
+		org: "OpenClassRooms",
+		url: "\certifOpen.PNG",
+		description: "",
 	}
 ]
 
 // Blogs --> #blogs section
-export const blogs = [
+export const veille = [
 	{
-		date: "12-July, 2023",
-		url: "https://medium.com/",
-		title: "Adventures in Safety Inspection",
-		description: "Join me in my humorous escapades as a safety inspector in Quahog.",
-		publisher: "Quahog Times",
+		title: "Benjamin Code",
+		source_name: "Youtube / Front-End, Design",
+		source: "https://www.youtube.com/c/BenjaminCode",
 	},
 	{
-		date: "25-June, 2023",
-		url: "https://medium.com/",
-		title: "Top 10 Safety Tips for Families",
-		description: "Learn the best safety practices for families to ensure a secure environment.",
-		publisher: "Safety Monthly",
+		title: "Grafikart",
+		source_name: "Youtube / Back-End",
+		source: "https://www.youtube.com/@grafikart",
+	},
+	{
+		title: "Micode",
+		source_name: "Youtube / IA, vulgarisation",
+		source: "https://www.youtube.com/channel/UCYnvxJ-PKiGXo_tYXpWAC-w",
+	},
+	{
+		title: "Artisan Développeur",
+		source_name: "Twitter / X / Back-End",
+		source: "https://twitter.com/art_developpeur",
+	},
+	{
+		description: "Ma veille s'axe sur essentiellement : l'IA; la cyber-sécurité; les crypto-monnaies et la blockchain; le front-end; le back-end",
+		source_name: "En savoir plus",
+		source: "maVeille.pdf",
 	}
 ]
 
 // Achievements --> #achievement section
-export const achievements = [
-	{
-		title: "Safety Hero Award",
-		year: "2020",
-		decription: "Recognized for outstanding contributions to improving safety standards in Quahog."
-	},
-	{
-		title: "Safety Innovation Prize",
-		year: "2018",
-		decription: "Awarded for implementing innovative safety measures that yielded significant improvements."
-	},
-	{
-		title: "Community Safety Ambassador",
-		year: "2015",
-		decription: "Honored for promoting safety awareness and education in the Quahog community."
-	},
-	{
-		title: "Safety Campaign Excellence",
-		year: "2013",
-		decription: "Acknowledged for leading a successful safety campaign that reached thousands of residents."
-	}
-]
+// export const achievements = [
+// 	{
+// 		title: "Safety Hero Award",
+// 		year: "2020",
+// 		decription: "Recognized for outstanding contributions to improving safety standards in Quahog."
+// 	},
+// 	{
+// 		title: "Safety Innovation Prize",
+// 		year: "2018",
+// 		decription: "Awarded for implementing innovative safety measures that yielded significant improvements."
+// 	},
+// 	{
+// 		title: "Community Safety Ambassador",
+// 		year: "2015",
+// 		decription: "Honored for promoting safety awareness and education in the Quahog community."
+// 	},
+// 	{
+// 		title: "Safety Campaign Excellence",
+// 		year: "2013",
+// 		decription: "Acknowledged for leading a successful safety campaign that reached thousands of residents."
+// 	}
+// ]
